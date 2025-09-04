c1d4068ab24d22d5b6110ff419898ea0aa53a830
Anthony <antnay@uw.edu>
2025-09-04 14:12:51 -0700

    Initial Commit

---
+++ drone/droneApp.swift
@@ -0,0 +1,17 @@
+//
+//  droneApp.swift
+//  drone
+//
+//  Created by Anthony on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct droneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
