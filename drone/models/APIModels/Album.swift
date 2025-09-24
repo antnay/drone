95985763617048a9a912dfb9063331d55d1da12e
Anthony <antnay@uw.edu>
2025-09-24 15:49:46 -0700

    fix: decode/encode album

--- drone/models/APIModels/Album.swift
+++ drone/models/APIModels/Album.swift
@@ -92,14 +92,20 @@ final class Album: Identifiable, Codable {
     var year: Int
     var genre: String
     var coverArt: String
-    var duration: Int
+    var duration: Float
     var artistId: String
     var artistImageUrl: String
     var musicBrainzId: String
     var sortName: String
     var displayAlbumArtist: String
 
-    init(id: String, albumId: String, parent: String, isDir: Bool, title: String, name: String, album: String, artist: String, year: Int, genre: String, coverArt: String, duration: Int, artistId: String, artistImageUrl: String, musicBrainzId: String, sortName: String, displayAlbumArtist: String) {
+    init(
+        id: String, albumId: String, parent: String, isDir: Bool, title: String,
+        name: String, album: String, artist: String, year: Int, genre: String,
+        coverArt: String, duration: Float, artistId: String,
+        artistImageUrl: String, musicBrainzId: String, sortName: String,
+        displayAlbumArtist: String
+    ) {
         self.id = id
         self.albumId = albumId
         self.parent = parent
@@ -121,55 +127,68 @@ final class Album: Identifiable, Codable {
 
     enum CodingKeys: String, CodingKey {
         case id
-//        var albumId: String
-//        var parent: String
-//        var isDir: Bool
-//        var title: String
-//        var name: String
-//        var album: String
-//        var artist: String
-//        var year: Int
-//        var genre: String
-//        var coverArt: String
-//        var duration: Int
-//        var artistId: String
-//        var artistImageUrl: String
-//        var musicBrainzId: String
-//        var sortName: String
-//        var displayAlbumArtist: String
+        case albumId
+        case parent
+        case isDir
+        case title
+        case name
+        case album
+        case artist
+        case year
+        case genre
+        case coverArt
+        case duration
+        case artistId
+        case artistImageUrl
+        case musicBrainzId
+        case sortName
+        case displayAlbumArtist
     }
 
     required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let values = try decoder.container(keyedBy: CodingKeys.self)
 
         self.id =
-            try container.decodeIfPresent(String.self, forKey: .id)
+            try values.decodeIfPresent(String.self, forKey: .id)
             ?? UUID().uuidString
-        self.albumId = try container.decode(String.self, forKey: .albumId)
-        self.artistID = try container.decode(String.self, forKey: .artistID)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.coverArt = try container.decode(String.self, forKey: .coverArt)
-        self.albumCount = try container.decode(Int.self, forKey: .albumCount)
-        self.artistImageUrl = try container.decode(
+        self.albumId = try values.decode(String.self, forKey: .albumId)
+        self.parent = try values.decode(String.self, forKey: .parent)
+        self.isDir = try values.decode(Bool.self, forKey: .isDir)
+        self.title = try values.decode(String.self, forKey: .title)
+        self.name = try values.decode(String.self, forKey: .name)
+        self.album = try values.decode(String.self, forKey: .album)
+        self.artist = try values.decode(String.self, forKey: .artist)
+        self.year = try values.decode(Int.self, forKey: .year)
+        self.genre = try values.decode(String.self, forKey: .genre)
+        self.coverArt = try values.decode(String.self, forKey: .coverArt)
+        self.duration = try values.decode(Float.self, forKey: .duration)
+        self.artistId = try values.decode(String.self, forKey: .artistId)
+        self.artistImageUrl = try values.decode(
             String.self, forKey: .artistImageUrl)
-        self.musicBrainzId = try container.decode(
+        self.musicBrainzId = try values.decode(
             String.self, forKey: .musicBrainzId)
-        self.sortName = try container.decode(String.self, forKey: .sortName)
-        self.roles = try container.decode([String].self, forKey: .roles)
+        self.sortName = try values.decode(String.self, forKey: .sortName)
+        self.displayAlbumArtist = try values.decode(
+            String.self, forKey: .displayAlbumArtist)
     }
 
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-
         try container.encode(id, forKey: .id)
-        try container.encode(albumId, forKey:
-        try container.encode(artistID, forKey: .artistID)
+        try container.encode(albumId, forKey: .albumId)
+        try container.encode(parent, forKey: .parent)
+        try container.encode(isDir, forKey: .isDir)
+        try container.encode(title, forKey: .title)
         try container.encode(name, forKey: .name)
+        try container.encode(album, forKey: .album)
+        try container.encode(artist, forKey: .artist)
+        try container.encode(year, forKey: .year)
+        try container.encode(genre, forKey: .genre)
         try container.encode(coverArt, forKey: .coverArt)
-        try container.encode(albumCount, forKey: .albumCount)
+        try container.encode(duration, forKey: .duration)
         try container.encode(artistImageUrl, forKey: .artistImageUrl)
         try container.encode(musicBrainzId, forKey: .musicBrainzId)
         try container.encode(sortName, forKey: .sortName)
-        try container.encode(roles, forKey: .roles)
+        try container.encode(displayAlbumArtist, forKey: .displayAlbumArtist)
     }
 }
